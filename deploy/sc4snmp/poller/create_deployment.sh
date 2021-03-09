b10d67892af3017164a7cb35bf5dc2db2f2665c3
lstoppa <lstoppa@splunk.com>
2021-03-09 11:17:39 +0100

    fix: added support in the deployment script for more components - added mongo support - when running the script we undeploy pretty much everything - we have now a single replica of everything (some services had 2)

--- deploy/sc4snmp/poller/create_deployment.sh
+++ deploy/sc4snmp/poller/create_deployment.sh
@@ -2,6 +2,12 @@
 
 KUBERNETES_POLLER_CONFIG_MAP_NAME="poller-config"
 
+kubernetes_undeploy_all() {
+  for conf in "$@"; do
+    kubectl delete -f "${conf}"
+  done
+}
+
 download_poller_config_file() {
   token=$1
   poller_config_raw_file="raw.githubusercontent.com/splunk/splunk-connect-for-snmp-poller/main/config.yaml"
@@ -38,6 +44,18 @@ kubernetes_create_or_replace_docker_secret() {
     --docker-email="${email}"
 }
 
+kubernetes_create_or_replace_hec_secret() {
+  url=$1
+  hec_token=$2
+  secret_name=$3
+
+  kubectl delete secret "${secret_name}"
+  kubectl create secret generic remote-splunk \
+    --from-literal=SPLUNK_HEC_URL="${url}" \
+    --from-literal=SPLUNK_HEC_TLS_VERIFY=no \
+    --from-literal=SPLUNK_HEC_TOKEN="${hec_token}"
+}
+
 clean_up() {
   for temporary_file in "$@"; do
     rm -rf "${temporary_file}"
@@ -59,9 +77,31 @@ kubernetes_deploy_rabbitmq() {
   kubectl create -f rq-service.yaml
 }
 
+kubernetes_deploy_mongo() {
+  kubectl create -f mongo-deployment.yaml
+  kubectl create -f mongo-service.yaml
+}
+
+kubernetes_deploy_poller() {
+  kubectl create -f scheduler-deployment.yaml
+  kubectl create -f worker-deployment.yaml
+}
+
+health_check() {
+  kubectl get pods
+  kubectl get service
+  kubectl get deployment
+  kubectl get secret
+}
 # -----------------------------------------------------------------------------
 # MAIN
 # -----------------------------------------------------------------------------
+
+kubernetes_undeploy_all worker-deployment.yaml scheduler-deployment.yaml \
+  mongo-deployment.yaml mongo-service.yaml \
+  rq-deployment.yaml rq-service.yaml
+health_check
+
 github_username="${USER}"
 github_email="${github_username}@splunk.com"
 echo "Please type your person access github token:"
@@ -71,7 +111,10 @@ kubernetes_poller_deploy_or_update_config "${poller_config_file}" "${KUBERNETES_
 # TODO: try to get the secret name with yq directly from scheduler-deployment.yaml. For now I am
 # getting a syntax error when trying to access a list, not sure why.
 kubernetes_create_or_replace_docker_secret "https://ghcr.io/v2/splunk" ${github_username} ${token} ${github_email} "regcred"
+kubernetes_create_or_replace_hec_secret "https://localhost:8000/services/collector" "12345678" "remote-splunk"
 kubernetes_deploy_rabbitmq
+kubernetes_deploy_mongo
+kubernetes_deploy_poller
 
 clean_up "${poller_config_file}"
-#undeploy_poller
+health_check
