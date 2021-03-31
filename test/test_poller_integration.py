d62badcafbc156ce915000f52e01da4cb485cf87
lstoppa <lstoppa@splunk.com>
2021-03-31 14:06:25 +0200

    feat: initial kubetest test for RabbitMQ - added testcase for RabbitMQ deployment - added testcase for RabbitMQ service - fixed an issue of our deployment files: if they end with "--"   the framework will fail because it interprets it as an empty   deployment/service definition (this is probably a kubetest's issue,   however I guess it is fine for now)

--- test/test_poller_integration.py
+++ test/test_poller_integration.py
@@ -13,5 +13,28 @@ def kubeconfig():
     return "~/.kube/config"
 
 
-def test_poller_integration(kube):
-    pass
+@pytest.fixture
+def rabbitmq_deployment(kube):
+    rabbitmq_deployment_yaml = "../deploy/sc4snmp/rq-deployment.yaml"
+    rq_deployment = kube.load_deployment(rabbitmq_deployment_yaml)
+    rq_deployment.create()
+    rq_deployment.wait_until_ready()
+    rq_deployment.refresh()
+    return rq_deployment
+
+
+@pytest.fixture
+def rabbitmq_service(kube):
+    rabbitmq_service_yaml = "../deploy/sc4snmp/rq-service.yaml"
+    rq_service = kube.load_service(rabbitmq_service_yaml)
+    rq_service.create()
+    rq_service.wait_until_ready()
+    rq_service.refresh()
+    return rq_service
+
+
+def test_poller_integration(rabbitmq_deployment, rabbitmq_service):
+    rq_deployment_pods = rabbitmq_deployment.get_pods()
+    assert len(rq_deployment_pods) == 1
+    rq_deployment_service = rabbitmq_service.get_endpoints()
+    assert len(rq_deployment_service) == 1
