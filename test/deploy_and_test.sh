5c0cc37dd65bf2a096e0970ca0c792ad0b4cad8e
lstoppa <lstoppa@splunk.com>
2021-04-07 16:47:37 +0200

    fix: stop the simulator deployment

--- test/deploy_and_test.sh
+++ test/deploy_and_test.sh
@@ -7,7 +7,7 @@ install_basic_software() {
     "sudo snap install docker" "sudo apt-get install snmp -y")
   for command in "${commands[@]}" ; do
     if ! ${command} ; then
-      echo "error when executing ${command}"
+      echo "Error when executing ${command}"
       exit 1
     fi
   done
@@ -28,6 +28,19 @@ deploy_kubernetes() {
   echo "For now I don't do anything"
 }
 
+stop_everything() {
+  id=$(sudo docker ps --filter ancestor=tandrup/snmpsim --format "{{.ID}}")
+  echo "Trying to stop docker container $id"
+
+  commands=("sudo docker stop $id" "sudo docker rm $id")
+  for command in "${commands[@]}" ; do
+    if ! ${command} ; then
+      echo "Error when executing ${command}"
+    fi
+  done
+}
+
 install_basic_software
 install_simulator
 deploy_kubernetes
+stop_everything
