fae74b8ad1042a68f2df140d807578a0f3a6a8ed
lstoppa <lstoppa@splunk.com>
2021-04-07 16:32:42 +0200

    feat: add support for simulator (fixed docker deployment)

--- test/deploy_and_test.sh
+++ test/deploy_and_test.sh
@@ -4,15 +4,24 @@ install_basic_software() {
   commands=("sudo snap install microk8s --classic" \
     "sudo microk8s status --wait-ready"  \
     "sudo microk8s enable dns helm3" \
-    "sudo snap install docker" "sudo apt-get install snmp -y" \
-    "nohup sudo docker run -p161:161/udp tandrup/snmpsim > /dev/null 2>&1 &" \
-    "snmpget -v1 -c public 127.0.0.1 1.3.6.1.2.1.1.1.0")
+    "sudo snap install docker" "sudo apt-get install snmp -y")
   for command in "${commands[@]}" ; do
     if ! ${command} ; then
       echo "error when executing ${command}"
       exit 1
     fi
   done
+}
+
+install_simulator() {
+  simulator=nohup sudo docker run -p161:161/udp tandrup/snmpsim > /dev/null 2>&1 &
+  if ! $simulator ; then
+    echo "Cannot start SNMP simulator"
+    exit 2
+  fi
+
+  echo "Sample SNMP get from the simulator"
+  snmpget -v1 -c public 127.0.0.1 1.3.6.1.2.1.1.1.0
 }
 
 deploy_kubernetes() {
