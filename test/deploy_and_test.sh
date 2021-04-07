0f04772c988ea6a64be01bb780e1c05477cffac9
lstoppa <lstoppa@splunk.com>
2021-04-07 16:20:22 +0200

    feat: add support for simulator

--- test/deploy_and_test.sh
+++ test/deploy_and_test.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-deploy_kubernetes() {
+install_basic_software() {
   commands=("sudo snap install microk8s --classic" \
     "sudo microk8s status --wait-ready"  \
-    "sudo microk8s enable dns helm3")
+    "sudo microk8s enable dns helm3" \
+    "sudo snap install docker" "sudo apt-get install snmp -y" \
+    "nohup sudo docker run -p161:161/udp tandrup/snmpsim > /dev/null 2>&1 &" \
+    "snmpget -v1 -c public 127.0.0.1 1.3.6.1.2.1.1.1.0")
   for command in "${commands[@]}" ; do
     if ! ${command} ; then
       echo "error when executing ${command}"
@@ -12,4 +15,9 @@ deploy_kubernetes() {
   done
 }
 
+deploy_kubernetes() {
+  echo "For now I don't do anything"
+}
+
+install_basic_software
 deploy_kubernetes
