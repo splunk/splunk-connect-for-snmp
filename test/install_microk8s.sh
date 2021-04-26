32284e7f6f3b483557e752a3423fb42d6ee39258
lstoppa <77723162+lstoppa@users.noreply.github.com>
2021-04-26 12:15:17 +0200

    feat: ADDON-35792 (#54)
    * feat: ADDON-35792
    - we can now run our integration tests on CentOS as well
    - this is mainly used to verify that we have a working
      Kubernetes running on CentOS, because as we know all
      the components are running on a separated container
    
    * feat: ADDON-35792
    - updated documentation with CentOS-specific instructions

---
+++ test/install_microk8s.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+setup_kube_roles() {
+  sudo usermod -a -G microk8s "$USER"
+  sudo chown -f -R "$USER" ~/.kube
+  echo "In order to properly configure Kubernetes, we need to logout the current shell. You might need to enter you password."
+  su - "$USER"
+}
+
+install_dependencies_on_ubuntu() {
+  sudo snap install microk8s --classic
+  sudo snap install docker
+  sudo apt-get install snmp -y
+  sudo apt-get install python3-dev -y
+}
+
+install_dependencies_on_centos() {
+  sudo yum -y install epel-release
+  sudo yum -y install snapd
+  sudo systemctl enable snapd
+  sudo systemctl start snapd
+  sudo ln -s /var/lib/snapd/snap /snap
+  sudo snap install microk8s --classic
+  sudo snap install docker
+  sudo yum install net-snmp net-snmp-utils -y
+  sudo yum install python3-devel -y
+}
+
+install_dependencies() {
+  os_release=/etc/os-release
+  if [ ! -f "$os_release" ] ; then
+    echo "$os_release does not exist"
+    exit 4
+  fi
+
+  os_version=$(grep "^ID=" "$os_release" | sed -e "s/\"//g" | cut -d= -f2)
+  if [ "$os_version" == "ubuntu" ] ; then
+    install_dependencies_on_ubuntu
+  elif [ "$os_version" == "centos" ] ; then
+    install_dependencies_on_centos
+  else
+    echo "Unsupported operating system: $os_version"
+    exit 4
+  fi
+
+  setup_kube_roles
+}
+
+install_dependencies
