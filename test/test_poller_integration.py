d2837bce9be901629cc5eed5033404c0b7b497af
lstoppa <lstoppa@splunk.com>
2021-05-06 14:34:06 +0200

    fix: rework the integration tests (#7) - fixed Splunk queries

--- test/test_poller_integration.py
+++ test/test_poller_integration.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 def test_poller_integration(setup_splunk):
     logger.info(f"Integration test for poller")
-    search_string = 'search index="snmp" earliest=-1m'
+    search_string = 'search index="em_logs" sourcetype="sc4snmp:meta" earliest=-1m'
     result_count, events_count = splunk_single_search(setup_splunk, search_string)
     assert result_count > 0
     assert events_count > 0
