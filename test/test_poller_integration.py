72d28c495e814f27edb9cd58d7ead51099bbf5f4
lstoppa <lstoppa@splunk.com>
2021-05-06 15:11:57 +0200

    fix: need to reformat the code

--- test/test_poller_integration.py
+++ test/test_poller_integration.py
@@ -1,4 +1,5 @@
 import logging
+
 from test.splunk_test_utils import splunk_single_search
 
 logger = logging.getLogger(__name__)
@@ -14,7 +15,7 @@ def test_poller_integration_event(setup_splunk):
 
 def test_poller_integration_metric(setup_splunk):
     logger.info(f"Integration test for poller")
-    search_string = '| mcatalog values(metric_name)  where index=em_metrics AND metric_name=sc4snmp.*'
+    search_string = "| mcatalog values(metric_name)  where index=em_metrics AND metric_name=sc4snmp.*"
     result_count, metric_count = splunk_single_search(setup_splunk, search_string)
     assert result_count > 0
     assert metric_count > 0
