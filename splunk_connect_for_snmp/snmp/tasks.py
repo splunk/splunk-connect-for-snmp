638a4d4a6bd48b3a129454748d613c48706c5124
rfaircloth-splunk <rfaircloth@splunk.com>
2021-11-15 17:30:53 -0500

    chore: refactor mib index
    Using a URI based index approach to allow for search from a static server

--- splunk_connect_for_snmp/snmp/tasks.py
+++ splunk_connect_for_snmp/snmp/tasks.py
@@ -31,18 +31,18 @@ from splunk_connect_for_snmp.common.requests import CachedLimiterSession
 
 logger = get_task_logger(__name__)
 
-MIB_SOURCES = os.getenv("MIB_SOURCES").split(";")
-MIB_INDEX = os.getenv("MIB_INDEX")
+MIB_SOURCES = os.getenv("MIB_SOURCES","https://pysnmp.github.io/mibs/asn1/@mib@")
+MIB_INDEX = os.getenv("MIB_INDEX","https://pysnmp.github.io/mibs/index/")
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB", "sc4")
 
 
-def load_mibs(mibs) -> None:
+def load_mibs(mibs: List[str]) -> None:
     mibBuilder = builder.MibBuilder()
     logger.info("This worker requires additional mibs")
     # Optionally attach PySMI MIB compiler (if installed)
     logger.debug("Attaching MIB compiler...")
-    compiler.addMibCompiler(mibBuilder, sources=MIB_SOURCES)
+    compiler.addMibCompiler(mibBuilder, sources=[MIB_SOURCES])
     logger.debug("Loading MIB modules..."),
     logger.info(f"loading mib modules {','.join(mibs)}")
     for mib in mibs:
@@ -108,203 +108,210 @@ def map_metric_type(t, snmp_value):
     return metric_type
 
 
-def isMIBKnown(id: str, oid: str) -> tuple([bool, List]):
-    session = CachedLimiterSession(
-        per_second=120,
-        cache_name="cache_http",
-        backend=MongoCache(url=MONGO_URI, db=MONGO_DB),
-        expire_after=300,
-        logger=logger,
-        match_headers=False,
-        stale_if_error=True,
-    )
-    index: dict = {}
-    mibs = []
-    found = False
-    response = session.request("GET", MIB_INDEX, timeout=90)
-    if response.status_code == 200:
-        logger.debug(response)
-        index = json.loads(response.content)
-    oid_list = tuple(oid.split("."))
-
-    if id.startswith("RFC1213-MIB::mib-"):
-        start = 4
-    elif id.startswith("SNMPv2-SMI::enterprises."):
-        start = 8
-    else:
-        start = 8
-
-    for i in range(len(oid_list), start, -1):
-        oid_to_check = ".".join(oid_list[:i])
-        logger.debug(f"Checking for {id} based on {oid_to_check}")
-        if oid_to_check in index["identity"]:
-            mibs = list(set(mibs + index["identity"][oid_to_check]))
-            found = True
-            break
-    logger.debug(f"MIB Finder {id} found={found} mibs={mibs}")
-    return found, mibs
-
-
-# @asyncio.coroutine
-def run_walk(id: str, profiles: List[str] = None, walk: bool = False):
-
-    mongo_client = pymongo.MongoClient(MONGO_URI)
-    targets_collection = mongo_client.sc4.targets
-    target = targets_collection.find_one(
-        {"_id": ObjectId(id)}, {"target": True, "config": {"community": True}}
-    )
-
-    if walk:
-        varBinds = [
-            ObjectType(
-                ObjectIdentity("1.3.6").addAsn1MibSource(
-                    "file:///Users/rfaircloth/splunk/github/GitHub/splunk-connect-for-snmp-mib-server/mibs/asn1",
-                )
-            ).loadMibs()
-        ]
-    else:
-        varBinds: list[ObjectType] = []
-        with open("config.yaml", "r") as file:
-            config_base = yaml.safe_load(file)
-
-        var_binds = []
-
-        for profile in profiles:
-            #TODO: Add profile name back to metric
-            if profile in config_base["poller"]["profiles"]:
-                for varBind in config_base["poller"]["profiles"][profile]["varBinds"]:
-                    logger.debug(f"varBind {varBind}")
-                    if len(varBind) == 3:
-                        varBinds.append(
-                            ObjectType(
-                                ObjectIdentity(
-                                    varBind[0], varBind[1], varBind[2]
-                                ).addAsn1MibSource(
-                                    "file:///Users/rfaircloth/splunk/github/GitHub/splunk-connect-for-snmp-mib-server/mibs/asn1/@mib@",
-                                )
-                            ).loadMibs()
-                        )
-                    elif len(varBind) == 2:
-                        varBinds.append(
-                            ObjectType(
-                                ObjectIdentity(varBind[0], varBind[1]).addAsn1MibSource(
-                                    "file:///Users/rfaircloth/splunk/github/GitHub/splunk-connect-for-snmp-mib-server/mibs/asn1/@mib@",
-                                )
-                            ).loadMibs()
-                        )
-                    else:
-                        continue
- 
-
-    logger.debug(target)
-    i = 0
-    metrics = OrderedDict()
-    retry = False
-    seedmibs = []
-    logger.debug(f"Walking {target} for {varBinds}")
-
-    target_address = target["target"].split(":")[0]
-    target_port = target["target"].split(":")[1]
-    if target["config"]["community"]["version"] in ("1", "2", "2c"):
-        communitydata = CommunityData(target["config"]["community"]["name"])
-    else:
-        raise NotImplementedError("version 3 not yet implemented")
-
-    snmpEngine = SnmpEngine()
-    # while True:
-    iterator = bulkCmd(
-        snmpEngine,
-        communitydata,
-        UdpTransportTarget((target_address, target_port)),
-        ContextData(),
-        0,
-        50,
-        *varBinds,
-    )
-    for errorIndication, errorStatus, errorIndex, varBindTable in iterator:
-
-        if errorIndication:
-            logger.error(errorIndication)
-            break
-        elif errorStatus:
-            logger.error(
-                "%s at %s"
-                % (
-                    errorStatus.prettyPrint(),
-                    errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
-                )
-            )
-            break
- 
-        else:
-            for varBind in varBindTable:
-                i += 1
-                logger.debug(varBind)
-                mib, metric, index = varBind[0].getMibSymbol()
-
-                id = varBind[0].prettyPrint()
-                oid = str(varBind[0].getOid())
-
-                logger.debug(f"{mib}.{metric} id={id} oid={oid}")
-
-                if isMIBResolved(id):
-                    group_key = get_group_key(mib, oid, index)
-                    if not group_key in metrics:
-                        metrics[group_key] = {
-                            "metrics": OrderedDict(),
-                            "fields": OrderedDict(),
-                        }
-
-                    snmp_val = varBind[1]
-                    snmp_type = type(snmp_val).__name__
-
-                    metric_type = map_metric_type(snmp_type, snmp_val)
-                    if metric_type in MTYPES:
-                        metrics[group_key]["metrics"][f"{mib}.{metric}"] = {
-                            "type": metric_type,
-                            "value": snmp_val.prettyPrint(),
-                            "oid": oid,
-                        }
-                    else:
-                        if not snmp_val.prettyPrint() == "":
-                            metrics[group_key]["fields"][f"{mib}.{metric}"] = {
-                                "type": metric_type,
-                                "value": snmp_val.prettyPrint(),
-                                "oid": oid,
-                            }
-
-                else:
-                    found, remotemibs = isMIBKnown(id, oid)
-                    if found:
-                        retry = True
-                        seedmibs = list(set(remotemibs + seedmibs))
-
-        # varBinds = varBindTable[-1]
-        # if isEndOfMib(varBinds):
-        #     logger.debug("Completed Walk")
-        #     break
-
-    snmpEngine.transportDispatcher.closeDispatcher()
-    if len(seedmibs) > 0:
-        load_mibs(seedmibs)
-    return retry, metrics
-
 
 class SNMPTask(Task):
     def __init__(self):
         # self.snmpEngine = SnmpEngine()
-
-        # self.session = CachedLimiterSession(
-        #     per_second=120,
-        #     cache_name="cache_http",
-        #     backend=MongoCache(url=MONGO_URI, db=MONGO_DB),
-        #     expire_after=300,
-        #     logger=logger,
-        #     match_headers=False,
-        #     stale_if_error=True,
-        # )
+       
+        self.session = CachedLimiterSession(
+            per_second=120,
+            cache_name="cache_http",
+            backend=MongoCache(url=MONGO_URI, db=MONGO_DB),
+            expire_after=1800,
+            #logger=logger,
+            match_headers=False,
+            stale_if_error=True,
+            allowable_codes=[200, 404]
+        )
+        load_mibs(["HOST-RESOURCES-MIB","IANAifType-MIB","IF-MIB","IP-MIB","RFC1389-MIB","SNMPv2-MIB","UCD-SNMP-MIB","TCP-MIB","UDP-MIB"])
+        self.snmpEngine = SnmpEngine()
         # self.index: dict = {}
-        pass
+        
+        
+
+
+
+    def isMIBKnown(self,id: str, oid: str) -> tuple([bool, List]):
+        
+        index: dict = {}
+        mibs = []
+        found = False
+        oid_list = tuple(oid.split("."))
+
+        if id.startswith("RFC1213-MIB::mib-"):
+            start = 6
+        elif id.startswith("SNMPv2-SMI::enterprises."):
+            start = 8
+        else:
+            start = 8
+
+        if start + 4>len(oid_list):
+            end = len(oid_list)
+        else:
+            end = start + 3
+        if start>end:
+            return False, []
+        for i in range(start,end, 1):
+            oid_to_check = "/".join(oid_list[:i])
+            
+            response = self.session.request("GET", f"{MIB_INDEX}{oid_to_check}/mib.txt", timeout=90)
+            if response.status_code == 200:
+                logger.debug(f"found for {id} based on {oid_to_check}")
+                logger.debug(response.content)
+                mibs = response.content.decode('utf-8').splitlines()
+                if "" in mibs:
+                    mibs.remove("")
+                found = True                
+            else:
+                return found, mibs
+        
+        return found, mibs
+
+
+    # @asyncio.coroutine
+    def run_walk(self,id: str, profiles: List[str] = None, walk: bool = False):
+
+        mongo_client = pymongo.MongoClient(MONGO_URI)
+        targets_collection = mongo_client.sc4.targets
+        target = targets_collection.find_one(
+            {"_id": ObjectId(id)}, {"target": True, "config": {"community": True}}
+        )
+
+        if walk:
+            varBinds = [
+                ObjectType(
+                    ObjectIdentity("1.3.6").addAsn1MibSource(
+                        MIB_SOURCES,
+                    )
+                ).loadMibs()
+            ]
+        else:
+            varBinds: list[ObjectType] = []
+            with open("config.yaml", "r") as file:
+                config_base = yaml.safe_load(file)
+
+            var_binds = []
+
+            for profile in profiles:
+                #TODO: Add profile name back to metric
+                if profile in config_base["poller"]["profiles"]:
+                    for varBind in config_base["poller"]["profiles"][profile]["varBinds"]:
+                        logger.debug(f"varBind {varBind}")
+                        if len(varBind) == 3:
+                            varBinds.append(
+                                ObjectType(
+                                    ObjectIdentity(
+                                        varBind[0], varBind[1], varBind[2]
+                                    ).addAsn1MibSource(
+                                        MIB_SOURCES,
+                                    )
+                                ).loadMibs()
+                            )
+                        elif len(varBind) == 2:
+                            varBinds.append(
+                                ObjectType(
+                                    ObjectIdentity(varBind[0], varBind[1]).addAsn1MibSource(
+                                        MIB_SOURCES,
+                                    )
+                                ).loadMibs()
+                            )
+                        else:
+                            continue
+    
+
+        logger.debug(target)
+        i = 0
+        metrics = OrderedDict()
+        retry = False
+        seedmibs = []
+        logger.debug(f"Walking {target} for {varBinds}")
+
+        target_address = target["target"].split(":")[0]
+        target_port = target["target"].split(":")[1]
+        if target["config"]["community"]["version"] in ("1", "2", "2c"):
+            communitydata = CommunityData(target["config"]["community"]["name"])
+        else:
+            raise NotImplementedError("version 3 not yet implemented")
+
+        
+        # while True:
+        iterator = bulkCmd(
+            self.snmpEngine,
+            communitydata,
+            UdpTransportTarget((target_address, target_port)),
+            ContextData(),
+            0,
+            50,
+            *varBinds,
+        )
+        for errorIndication, errorStatus, errorIndex, varBindTable in iterator:
+
+            if errorIndication:
+                logger.error(errorIndication)
+                break
+            elif errorStatus:
+                logger.error(
+                    "%s at %s"
+                    % (
+                        errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+                    )
+                )
+                break
+    
+            else:
+                for varBind in varBindTable:
+                    i += 1
+                    #logger.debug(varBind)
+                    mib, metric, index = varBind[0].getMibSymbol()
+
+                    id = varBind[0].prettyPrint()
+                    oid = str(varBind[0].getOid())
+
+                    #logger.debug(f"{mib}.{metric} id={id} oid={oid}")
+
+                    if isMIBResolved(id):
+                        group_key = get_group_key(mib, oid, index)
+                        if not group_key in metrics:
+                            metrics[group_key] = {
+                                "metrics": OrderedDict(),
+                                "fields": OrderedDict(),
+                            }
+
+                        snmp_val = varBind[1]
+                        snmp_type = type(snmp_val).__name__
+
+                        metric_type = map_metric_type(snmp_type, snmp_val)
+                        if metric_type in MTYPES:
+                            metrics[group_key]["metrics"][f"{mib}.{metric}"] = {
+                                "type": metric_type,
+                                "value": snmp_val.prettyPrint(),
+                                "oid": oid,
+                            }
+                        else:
+                            if not snmp_val.prettyPrint() == "":
+                                metrics[group_key]["fields"][f"{mib}.{metric}"] = {
+                                    "type": metric_type,
+                                    "value": snmp_val.prettyPrint(),
+                                    "oid": oid,
+                                }
+
+                    else:
+                        found, remotemibs = self.isMIBKnown(id, oid)
+                        if found:
+                            retry = True
+                            seedmibs = list(set(remotemibs + seedmibs))
+                            break
+
+            # varBinds = varBindTable[-1]
+            # if isEndOfMib(varBinds):
+            #     logger.debug("Completed Walk")
+            #     break
+
+        #self.snmpEngine.transportDispatcher.closeDispatcher()
+        if len(seedmibs) > 0:
+            load_mibs(seedmibs)
+        return retry, metrics
 
 
 @shared_task(bind=True, base=SNMPTask)
@@ -314,7 +321,7 @@ def walk(self, **kwargs):
     retry = True
     now = datetime.utcnow().timestamp()
     while retry:
-        retry, result = run_walk(
+        retry, result = self.run_walk(
             kwargs["id"],
             walk=True,
         )
@@ -336,7 +343,7 @@ def poll(self, **kwargs):
 
 
     # After a Walk tell schedule to recalc
-    retry, result = run_walk(
+    retry, result = self.run_walk(
         kwargs["id"],
         profiles=kwargs["profiles"],
     )
