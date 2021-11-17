c2952b8b2db5e276339531fd1b8f3824c51a7c14
rfaircloth-splunk <rfaircloth@splunk.com>
2021-11-17 08:30:45 -0500

    fix: make logging level configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,24 @@
 set -e
 echo args "$@"
 . /venv/bin/activate
-
+LOG_LEVEL=${LOG_LEVEL:=INFO}
 case $1 in
 
 celery)
     case $2 in
     beat)
-        celery -A splunk_connect_for_snmp.poller beat  --loglevel=INFO
+        celery -A splunk_connect_for_snmp.poller beat  --loglevel=$LOG_LEVEL
         ;;
     worker)
-        celery -A splunk_connect_for_snmp.poller worker --loglevel=INFO
+        celery -A splunk_connect_for_snmp.poller worker --loglevel=$LOG_LEVEL
         ;;
     *)
-        celery -A splunk_connect_for_snmp.poller "${@:3}" --loglevel=INFO
+        celery -A splunk_connect_for_snmp.poller "${@:3}" --loglevel=$LOG_LEVEL
         ;;
     esac
     ;;
 trap)
-    python -m splunk_connect_for_snmp.traps
+    python -m splunk_connect_for_snmp.traps --loglevel=$LOG_LEVEL
     ;;
 *)
 echo -n unknown cmd "$@"
