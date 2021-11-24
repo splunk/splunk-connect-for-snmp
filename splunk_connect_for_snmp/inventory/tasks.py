c6bd5d550aca2d7e8ad5420b05451f9fb6a7e595
weliasz <weliasz@splunk.com>
2021-11-24 12:56:51 +0100

    feat: cleanup of logging in the application

--- splunk_connect_for_snmp/inventory/tasks.py
+++ splunk_connect_for_snmp/inventory/tasks.py
@@ -7,18 +7,18 @@ except:
 import csv
 import os
 import re
-from typing import List, Union
+from typing import List
 
 import pymongo
 import urllib3
 import yaml
 from bson.objectid import ObjectId
 from celery import shared_task
-from celery.canvas import chain, chord, group, signature
+from celery.canvas import chain, group, signature
 from celery.utils.log import get_task_logger
 
 from splunk_connect_for_snmp import customtaskmanager
-from splunk_connect_for_snmp.common.hummanbool import hummanBool
+from splunk_connect_for_snmp.common.hummanbool import human_bool
 from splunk_connect_for_snmp.common.inventory_record import InventoryRecord
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -59,8 +59,9 @@ def inventory_seed(path=None):
             except:
                 ir.walk_interval = 42000
 
-            if "delete" in target and hummanBool(ir.delete, default=False):
+            if "delete" in target and human_bool(ir.delete, default=False):
                 periodic_obj.delete_task(ir.address)
+                logger.info(f"Deleting device: {ir.address}")
             else:
                 if ir.version not in ("1", "2", "2c", "3"):
                     logger.error("Invalid version in inventory record {row}")
@@ -95,7 +96,7 @@ def inventory_seed(path=None):
                 if ir.profiles:
                     profiles = ir.profiles
 
-                SmartProfiles: bool = hummanBool(ir.SmartProfiles, default=True)
+                SmartProfiles: bool = human_bool(ir.SmartProfiles, default=True)
 
                 updates.append(
                     {
@@ -112,6 +113,7 @@ def inventory_seed(path=None):
                 ur = targets_collection.update_one(
                     {"target": ir.address}, updates, upsert=True
                 )
+                logger.debug(f"Updating target={ir.address} with updates={updates}")
                 fr = targets_collection.find_one({"target": ir.address}, {"_id": True})
                 task_config = {
                     "name": f"sc4snmp;{ir.address};walk",
@@ -149,7 +151,7 @@ def inventory_seed(path=None):
 
                 if ur.modified_count:
                     logger.debug("Device Config Changed need to walk")
-                    logger.info(f"Upserted id: {fr['_id']}")
+                    logger.debug(f"Upserted id: {fr['_id']}")
                     task_config["kwargs"]["id"] = str(fr["_id"])
                     task_config["run_immediately"] = True
                 else:
@@ -180,7 +182,7 @@ def inventory_setup_poller(work):
             logger.debug(f"Checking match for {profile_name} {profile}")
 
             # Skip this profile its disabled
-            if hummanBool(profile.get("disabled", False), default=False):
+            if human_bool(profile.get("disabled", False), default=False):
                 logger.debug(f"Skipping disabled profile {profile_name}")
                 continue
 
