ebd65724c3f5cdc7a564d56af858be82ffa5c98a
omrozowicz-splunk <86965961+omrozowicz-splunk@users.noreply.github.com>
2021-11-25 17:53:10 +0100

    fix: next update helm chart (#171)
    * fix: update helm chart
    
    * fix: add inventory to worker pod
    
    * fix: [experimental] add inventory to traps pod
    
    * fix: change trap entrypoint
    
    * fix: add env variables to worker
    
    * fix: change config.yaml
    
    * fix: fix typo in env variables
    
    * fix: change env values to not be gathered from secrets
    
    * fix: delete b64enc from tls verify
    
    * fix: comment out traps
    
    * fix: use inventory file from env variables
    
    * fix: clean kubernetes yaml files
    
    * fix: delete otel and traps
    
    * fix: update traps ports
    
    * fix: delete otel
    
    * fix: update HELM chart
    
    * fix: fix issue with no inventory_refresh_rate provided
    
    * fix: delete inventory from worker
    
    * fix: update Chart version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
-echo args "$@"
 . /app/.venv/bin/activate
 LOG_LEVEL=${LOG_LEVEL:=INFO}
-
 wait-for-dep "${CELERY_BROKER_URL}"
 wait-for-dep "${MONGO_URI}"
 
@@ -13,18 +11,18 @@ case $1 in
 celery)
     case $2 in
     beat)
-        celery -A splunk_connect_for_snmp.poller beat  --loglevel=$LOG_LEVEL
+        celery -A splunk_connect_for_snmp.poller beat -l "$LOG_LEVEL"
         ;;
     worker)
-        celery -A splunk_connect_for_snmp.poller worker --loglevel=$LOG_LEVEL
+        celery -A splunk_connect_for_snmp.poller worker -l "$LOG_LEVEL"
         ;;
     *)
-        celery -A splunk_connect_for_snmp.poller "${@:3}" --loglevel=$LOG_LEVEL
+        celery -A splunk_connect_for_snmp.poller "${@:3}" -l "$LOG_LEVEL"
         ;;
     esac
     ;;
 trap)
-    python -m splunk_connect_for_snmp.traps --loglevel=$LOG_LEVEL
+    python -m splunk_connect_for_snmp.traps -l "$LOG_LEVEL"
     ;;
 *)
 echo -n unknown cmd "$@"
