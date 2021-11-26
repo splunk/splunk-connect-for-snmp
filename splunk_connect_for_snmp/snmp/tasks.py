6eab87c206cc446242494e6664885286b4a0f311
omrozowicz-splunk <omrozowicz@splunk.com>
2021-11-26 13:38:13 +0100

    fix: change config.yaml to use env variable

--- splunk_connect_for_snmp/snmp/tasks.py
+++ splunk_connect_for_snmp/snmp/tasks.py
@@ -56,6 +56,7 @@ MIB_SOURCES = os.getenv("MIB_SOURCES", "https://pysnmp.github.io/mibs/asn1/@mib@
 MIB_INDEX = os.getenv("MIB_INDEX", "https://pysnmp.github.io/mibs/index.csv")
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB", "sc4snmp")
+CONFIG_PATH = os.getenv("CONFIG_PATH", "/work/config/config.yaml")
 
 
 def valueAsBest(value) -> Union[str, float]:
@@ -227,7 +228,7 @@ class SNMPTask(Task):
         metrics = {}
         retry = False
 
-        with open("config.yaml") as file:
+        with open(CONFIG_PATH) as file:
             config_base = yaml.safe_load(file)
 
         # Connection and Security setup
