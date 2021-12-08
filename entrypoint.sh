6911be08d74105561b5646aaaebdfaada9d95355
omrozowicz-splunk <omrozowicz@splunk.com>
2021-12-08 14:26:27 +0100

    fix: update concurrency value in worker deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 . /app/.venv/bin/activate
 LOG_LEVEL=${LOG_LEVEL:=INFO}
+WORKER_CONCURRENCY=${WORKER_CONCURRENCY:=2}
 wait-for-dep "${CELERY_BROKER_URL}" "${MONGO_URI}" "${MIB_INDEX}"
 
 case $1 in
@@ -13,7 +14,7 @@ celery)
         celery -A splunk_connect_for_snmp.poller beat -l "$LOG_LEVEL"
         ;;
     worker)
-        celery -A splunk_connect_for_snmp.poller worker -l "$LOG_LEVEL" --concurrency=2 -O fair
+        celery -A splunk_connect_for_snmp.poller worker -l "$LOG_LEVEL" --concurrency="$WORKER_CONCURRENCY" -O fair
         ;;
     *)
         celery -A splunk_connect_for_snmp.poller "${@:3}" -l "$LOG_LEVEL"
