1073a82fcb1a94f58a31dda0e13d049909a3ed8a
omrozowicz-splunk <omrozowicz@splunk.com>
2021-12-23 14:33:43 +0100

    test: change assertEqual to assertTrue/assertFalse

--- test/common/test_humanbool.py
+++ test/common/test_humanbool.py
@@ -5,30 +5,30 @@ from splunk_connect_for_snmp.common.hummanbool import human_bool
 
 class TestHumanBool(TestCase):
     def test_human_bool_true(self):
-        self.assertEqual(human_bool("t"), True)
-        self.assertEqual(human_bool("T"), True)
-        self.assertEqual(human_bool("True"), True)
-        self.assertEqual(human_bool("true"), True)
-        self.assertEqual(human_bool("TrUe"), True)
-        self.assertEqual(human_bool("y"), True)
-        self.assertEqual(human_bool("yes"), True)
-        self.assertEqual(human_bool("1"), True)
-        self.assertEqual(human_bool(True), True)
+        self.assertTrue(human_bool("t"))
+        self.assertTrue(human_bool("T"))
+        self.assertTrue(human_bool("True"))
+        self.assertTrue(human_bool("true"))
+        self.assertTrue(human_bool("TrUe"))
+        self.assertTrue(human_bool("y"))
+        self.assertTrue(human_bool("yes"))
+        self.assertTrue(human_bool("1"))
+        self.assertTrue(human_bool(True))
 
     def test_human_bool_false(self):
-        self.assertEqual(human_bool("f"), False)
-        self.assertEqual(human_bool("F"), False)
-        self.assertEqual(human_bool("False"), False)
-        self.assertEqual(human_bool("fAlSe"), False)
-        self.assertEqual(human_bool("n"), False)
-        self.assertEqual(human_bool("no"), False)
-        self.assertEqual(human_bool("0"), False)
-        self.assertEqual(human_bool(False), False)
+        self.assertFalse(human_bool("f"))
+        self.assertFalse(human_bool("F"))
+        self.assertFalse(human_bool("False"))
+        self.assertFalse(human_bool("fAlSe"))
+        self.assertFalse(human_bool("n"))
+        self.assertFalse(human_bool("no"))
+        self.assertFalse(human_bool("0"))
+        self.assertFalse(human_bool(False))
 
     def test_human_bool_none(self):
-        self.assertEqual(human_bool(flag=None), False)
+        self.assertFalse(human_bool(flag=None))
 
     def test_human_bool_default(self):
-        self.assertEqual(human_bool("foo", True), True)
-        self.assertEqual(human_bool("1foo", False), False)
-        self.assertEqual(human_bool("1FoO"), False)
+        self.assertTrue(human_bool("foo", True))
+        self.assertFalse(human_bool("1foo", False))
+        self.assertFalse(human_bool("1FoO"))
