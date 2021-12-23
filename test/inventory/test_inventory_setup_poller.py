80892acd8c4cd14345226d346d07b58ad0b585d8
weliasz <weliasz@splunk.com>
2021-12-23 09:34:45 +0100

    chore: completed tests for inventory module

--- test/inventory/test_inventory_setup_poller.py
+++ test/inventory/test_inventory_setup_poller.py
@@ -1,68 +1,56 @@
 from unittest import TestCase, mock
-from unittest.mock import patch, mock_open, Mock
-import pymongo
+from unittest.mock import patch, Mock
 
-import splunk_connect_for_snmp
 from splunk_connect_for_snmp.common.inventory_record import InventoryRecord
-from splunk_connect_for_snmp.inventory.tasks import generate_poll_task_definition, inventory_setup_poller, MONGO_URI, \
-    MONGO_DB, assign_profiles
-from splunk_connect_for_snmp.snmp.manager import get_inventory
+from splunk_connect_for_snmp.inventory.tasks import generate_poll_task_definition, inventory_setup_poller
 
 
-# def test_inventory_setup_poller(self, m_assign_profiles, m_find_one, m_get_inventory, m_taskManager):
 class TestInventorySetupPoller(TestCase):
-    # @patch('splunk_connect_for_snmp.customtaskmanager.CustomPeriodicTaskManager')
-    # @mock.patch("splunk_connect_for_snmp.snmp.manager.get_inventory")
-    # @mock.patch("pymongo.collection.Collection.find_one")
-    # @mock.patch("splunk_connect_for_snmp.inventory.tasks.assign_profiles")
-    @mock.patch("splunk_connect_for_snmp.snmp.manager.get_inventory")
-    def test_inventory_setup_poller(self, m_get_inventory):
-        # periodic_obj_mock = Mock()
-        # m_taskManager.return_value = periodic_obj_mock
-        m_get_inventory.return_value = {"asd": 12}
+    @mock.patch('splunk_connect_for_snmp.common.profiles.load_profiles')
+    @patch('splunk_connect_for_snmp.customtaskmanager.CustomPeriodicTaskManager')
+    @mock.patch("pymongo.collection.Collection.find_one")
+    @mock.patch("splunk_connect_for_snmp.inventory.tasks.assign_profiles")
+    @mock.patch("splunk_connect_for_snmp.inventory.tasks.get_inventory")
+    def test_inventory_setup_poller(self, m_get_inventory, m_assign_profiles, m_find_one, m_task_manager, m_load_profiles):
+        periodic_obj_mock = Mock()
+        m_task_manager.return_value = periodic_obj_mock
 
-        #result = get_inventory(None, None)
+        m_get_inventory.return_value = InventoryRecord.from_dict({
+            "address": "192.168.0.1",
+            "port": "34",
+            "version": "2c",
+            "community": "public",
+            "secret": "secret",
+            "securityEngine": "ENGINE",
+            "walk_interval": 1850,
+            "profiles": "",
+            "SmartProfiles": True,
+            "delete": False,
+        })
 
-        mongo_client = pymongo.MongoClient(MONGO_URI)
-        mongo_db = mongo_client[MONGO_DB]
+        m_find_one.return_value = {"state":
+                      {"SNMPv2-MIB|sysDescr": {"value": "MIKROTIK"},
+                       "SNMPv2-MIB|sysName": {"value": "Linux Debian 2.0.1"},
+                       "SNMPv2-MIB|sysContact": {"value": "non-existing-name@splunk"}}}
 
-        mongo_inventory = mongo_db.inventory
-        targets_collection = mongo_db.targets
-
-        #ir = get_inventory(mongo_inventory, "192.168.0.1")
-
-
-        # m_get_inventory.return_value = InventoryRecord.from_dict({
-        #     "address": "192.168.0.1",
-        #     "port": "34",
-        #     "version": "2c",
-        #     "community": "public",
-        #     "secret": "secret",
-        #     "securityEngine": "ENGINE",
-        #     "walk_interval": 1850,
-        #     "profiles": "",
-        #     "SmartProfiles": True,
-        #     "delete": False,
-        # })
-
-        # m_find_one.return_value = {"state":
-        #               {"SNMPv2-MIB|sysDescr": {"value": "MIKROTIK"},
-        #                "SNMPv2-MIB|sysName": {"value": "Linux Debian 2.0.1"},
-        #                "SNMPv2-MIB|sysContact": {"value": "non-existing-name@splunk"}}}
-        #
         work = {"address": "192.168.0.1"}
-        #
-        # m_assign_profiles.return_value = {60: ['BaseUpTime'], 30: ['profile5', 'profile2'], 20: ['profile1']}
+
+        m_assign_profiles.return_value = {60: ['BaseUpTime'], 30: ['profile5', 'profile2'], 20: ['profile1']}
 
         # when
         inventory_setup_poller(work)
 
-        # load_profiles() should be called
+        m_load_profiles.assert_not_called()
 
-        #periodic_obj_mock.manage_task.assert_called_with({})
+        calls = periodic_obj_mock.manage_task.call_args_list
 
-        #periodic_obj_mock.delete_unused_poll_tasks.assert_called_with({})
-        #periodic_obj_mock.delete_disabled_poll_tasks.assert_called()
+        self.assertEqual({'address': '192.168.0.1', 'profiles': {'BaseUpTime'}, 'frequency': 60}, calls[0][1]["kwargs"])
+        self.assertEqual({'address': '192.168.0.1', 'profiles': {'profile2', 'profile5'}, 'frequency': 30}, calls[1][1]["kwargs"])
+        self.assertEqual({'address': '192.168.0.1', 'profiles': {'profile1'}, 'frequency': 20}, calls[2][1]["kwargs"])
+
+        periodic_obj_mock.delete_unused_poll_tasks.assert_called_with("192.168.0.1",
+                       ['sc4snmp;192.168.0.1;60;poll', 'sc4snmp;192.168.0.1;30;poll', 'sc4snmp;192.168.0.1;20;poll'])
+        periodic_obj_mock.delete_disabled_poll_tasks.assert_called()
 
     def test_generate_poll_task_definition(self):
         active_schedules = []
