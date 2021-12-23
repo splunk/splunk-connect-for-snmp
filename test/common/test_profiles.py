c217c8b8f74aafdf588d566ff7fdfda574bb8700
omrozowicz-splunk <omrozowicz@splunk.com>
2021-12-23 16:24:36 +0100

    test: add mechanic of mocking yaml files

---
+++ test/common/test_profiles.py
@@ -0,0 +1,36 @@
+from unittest import TestCase, mock
+import os
+
+from splunk_connect_for_snmp.common.profiles import load_profiles
+
+
+def return_yaml_profiles(args=None):
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "base_profiles", "base.yaml"
+    )
+    return [path]
+
+
+def return_config(*args):
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "base_profiles", "runtime_config.yaml"
+    )
+    return path
+
+
+class TestProfiles(TestCase):
+
+    def test_file_not_found(self):
+        pass
+
+    @mock.patch('splunk_connect_for_snmp.common.profiles.os.listdir', return_yaml_profiles)
+    @mock.patch('splunk_connect_for_snmp.common.profiles.CONFIG_PATH', return_config())
+    def test_read_base_profiles(self):
+        active_profiles = {'BaseUpTime': {'frequency': 300, 'condition': {'type': 'base'}, 'varBinds': [['IF-MIB', 'ifName'], ['IF-MIB', 'ifAlias'], ['SNMPv2-MIB', 'sysUpTime', 0]]}, 'EnirchIF': {'frequency': 600, 'condition': {'type': 'base'}, 'varBinds': [['IF-MIB', 'ifDescr'], ['IF-MIB', 'ifAdminStatus'], ['IF-MIB', 'ifName'], ['IF-MIB', 'ifAlias']]}}
+        self.assertEqual(load_profiles(), active_profiles)
+
+    def test_runtime_profiles(self):
+        pass
+
+    def test_all_profiles(self):
+        pass
