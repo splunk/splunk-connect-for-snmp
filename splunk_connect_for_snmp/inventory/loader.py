9157368e93e64acafe34630dfbd9b42f24b862e2
weliasz <weliasz@splunk.com>
2022-01-12 07:01:38 +0100

    fix: handle big enrichment objects

--- splunk_connect_for_snmp/inventory/loader.py
+++ splunk_connect_for_snmp/inventory/loader.py
@@ -43,6 +43,8 @@ MONGO_DB = os.getenv("MONGO_DB", "sc4snmp")
 CONFIG_PATH = os.getenv("CONFIG_PATH", "/app/config/config.yaml")
 INVENTORY_PATH = os.getenv("INVENTORY_PATH", "/app/inventory/inventory.csv")
 
+CURRENT_SCHEMA_VERSION = 1
+
 
 def gen_walk_task(ir: InventoryRecord):
     return {
@@ -73,6 +75,26 @@ def gen_walk_task(ir: InventoryRecord):
     }
 
 
+def migrate_database():
+    # fetch current schema version
+    previous_current_schema_version = 0
+
+    for x in range(previous_current_schema_version, CURRENT_SCHEMA_VERSION):
+        eval("migrate_to_version_" + str(x+1) + "()")
+
+
+def migrate_to_version_1():
+    logger.info("Migrating database schema to version 1")
+
+    task_manager = customtaskmanager.CustomPeriodicTaskManager()
+    mongo_client = pymongo.MongoClient(MONGO_URI)
+    targets_collection = mongo_client.sc4snmp.targets
+
+    task_manager.delete_all_poll_tasks()
+    targets_collection.update({}, {"$unset": {"attributes": 1}}, False, True)
+    task_manager.rerun_all_walks()
+
+
 def load():
     path = INVENTORY_PATH
     inventory_errors = False
@@ -82,6 +104,8 @@ def load():
     inventory_records = mongo_db.inventory
 
     periodic_obj = customtaskmanager.CustomPeriodicTaskManager()
+
+    migrate_database()
 
     logger.info(f"Loading inventory from {path}")
     with open(path, encoding='utf-8') as csv_file:
