72a8720e5b0ae35991a06304e82d45b1a9836e94
weliasz <weliasz@splunk.com>
2022-01-14 11:22:06 +0100

    tests: next step

--- integration_tests/prepare_splunk.sh
+++ integration_tests/prepare_splunk.sh
@@ -1,16 +1,18 @@
 create_splunk_indexes() {
-  splunk_ip=$1
-  splunk_password=$2
   index_names=("netmetrics" "netops")
   index_types=("metric" "event")
   for index in "${!index_names[@]}" ; do
-    if ! curl -k -u admin:"changeme" "https://localhost:8089/services/data/indexes" \
+    if ! curl -k -u admin:"changeme2" "https://localhost:8089/services/data/indexes" \
       -d datatype="${index_types[${index}]}" -d name="${index_names[${index}]}" ; then
       echo "Error when creating ${index_names[${index}]} of type ${index_types[${index}]}"
     fi
   done
 }
 
-
+create_splunk_hec() {
+  if ! curl -k -u admin:changeme2 https://localhost:8089/servicesNS/admin/splunk_httpinput/data/inputs/http -d name=new_token | grep "token" | cut -c 29-64 > hec_token ; then
+    echo "Error when creating ${index_names[${index}]} of type ${index_types[${index}]}"
+  fi
+}
 
 create_splunk_indexes
