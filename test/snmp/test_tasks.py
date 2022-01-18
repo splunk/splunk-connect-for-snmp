278db9452200752781ced68cf58693271b69b516
omrozowicz-splunk <omrozowicz@splunk.com>
2022-01-18 13:05:59 +0100

    fix: fix unit tests mongo mocking

--- test/snmp/test_tasks.py
+++ test/snmp/test_tasks.py
@@ -1,21 +1,20 @@
-from unittest import TestCase, mock
+from unittest import TestCase
 from unittest.mock import Mock, patch, MagicMock
 
 from pysnmp.smi.error import SmiError
 
-from splunk_connect_for_snmp.snmp.tasks import walk, poll, trap
 
-
+@patch('pymongo.MongoClient')
+@patch('mongolock.MongoLock.__init__')
+@patch('mongolock.MongoLock.lock')
+@patch('mongolock.MongoLock.release')
 class TestTasks(TestCase):
 
-    @patch('pymongo.MongoClient')
-    @patch('mongolock.MongoLock.__init__')
-    @patch('mongolock.MongoLock.lock')
-    @patch('mongolock.MongoLock.release')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.__init__')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.do_work')
     @patch('time.time')
     def test_walk(self, m_time, m_do_work, m_poller, m_release, m_lock, m_mongo_lock, m_mongo_client):
+        from splunk_connect_for_snmp.snmp.tasks import walk
         m_mongo_client.return_value = Mock()
         m_mongo_lock.return_value = None
         m_time.return_value = 1640692955.365186
@@ -32,14 +31,11 @@ class TestTasks(TestCase):
 
         self.assertEqual({'time': 1640692955.365186, 'address': '192.168.0.1', 'result': {'test': 'value1'}}, result)
 
-    @patch('pymongo.MongoClient')
-    @patch('mongolock.MongoLock.__init__')
-    @patch('mongolock.MongoLock.lock')
-    @patch('mongolock.MongoLock.release')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.__init__')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.do_work')
     @patch('time.time')
     def test_poll(self, m_time, m_do_work, m_poller, m_release, m_lock, m_mongo_lock, m_mongo_client):
+        from splunk_connect_for_snmp.snmp.tasks import poll
         m_mongo_client.return_value = Mock()
         m_mongo_lock.return_value = None
         m_time.return_value = 1640692955.365186
@@ -59,7 +55,8 @@ class TestTasks(TestCase):
     @patch('pysnmp.smi.rfc1902.ObjectType.resolveWithMib')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.process_snmp_data')
     @patch('time.time')
-    def test_trap(self, m_time, m_process_data, m_resolved):
+    def test_trap(self, m_time, m_process_data, m_resolved,  *mongo_args):
+        from splunk_connect_for_snmp.snmp.tasks import trap
         m_time.return_value = 1640692955.365186
 
         m_resolved.return_value = None
@@ -81,7 +78,8 @@ class TestTasks(TestCase):
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.is_mib_known')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.load_mibs')
     @patch('time.time')
-    def test_trap_retry_translation(self, m_time, m_load_mib, m_is_mib_known, m_process_data, m_resolved):
+    def test_trap_retry_translation(self, m_time, m_load_mib, m_is_mib_known, m_process_data, m_resolved,  *mongo_args):
+        from splunk_connect_for_snmp.snmp.tasks import trap
         m_time.return_value = 1640692955.365186
 
         m_resolved.side_effect = [SmiError, "TEST1"]
@@ -110,7 +108,8 @@ class TestTasks(TestCase):
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.is_mib_known')
     @patch('splunk_connect_for_snmp.snmp.manager.Poller.load_mibs')
     @patch('time.time')
-    def test_trap_retry_translation_failed(self, m_time, m_load_mib, m_is_mib_known, m_process_data, m_resolved):
+    def test_trap_retry_translation_failed(self, m_time, m_load_mib, m_is_mib_known, m_process_data, m_resolved, *mongo_args):
+        from splunk_connect_for_snmp.snmp.tasks import trap
         m_time.return_value = 1640692955.365186
 
         m_resolved.side_effect = [SmiError, SmiError]
@@ -120,6 +119,7 @@ class TestTasks(TestCase):
         m_process_data.return_value = (False, [], {"test": "value1"})
         self_obj = MagicMock()
         self_obj.trap = trap
+        self_obj.mib_view_controller = MagicMock()
         result = self_obj.trap(work)
 
         calls = m_load_mib.call_args_list
