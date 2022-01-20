f168af96b6c85a713b5f2a502d53b2dfe020e754
weliasz <77732905+weliasz@users.noreply.github.com>
2022-01-20 19:00:16 +0100

    test: more unit tests (#360)
    * test: auth tests
    
    * test: utils tests
    
    * test: poller test
    
    * test: more enricher tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests
    
    * test: poller & trap tests

---
+++ test/test_poller.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from splunk_connect_for_snmp.poller import init_celery_tracing, init_celery_beat_tracing, setup_task_logger
+
+
+class TestPoller(TestCase):
+    @patch('opentelemetry.instrumentation.celery.CeleryInstrumentor.instrument')
+    @patch('opentelemetry.instrumentation.logging.LoggingInstrumentor.instrument')
+    def test_init_celery_tracing(self, m_instr1, m_instr2):
+        init_celery_tracing()
+        m_instr1.assert_called()
+        m_instr2.assert_called()
+
+    @patch('opentelemetry.instrumentation.celery.CeleryInstrumentor.instrument')
+    @patch('opentelemetry.instrumentation.logging.LoggingInstrumentor.instrument')
+    def test_init_celery_beat_tracing(self, m_instr1, m_instr2):
+        init_celery_beat_tracing()
+        m_instr1.assert_called()
+        m_instr2.assert_called()
+
+    def test_setup_task_logger(self):
+        logger = MagicMock()
+        handler1 = MagicMock()
+        handler2 = MagicMock()
+        logger.handlers = [handler1, handler2]
+        setup_task_logger(logger)
+
+        handler1.setFormatter.assert_called()
+        handler2.setFormatter.assert_called()
