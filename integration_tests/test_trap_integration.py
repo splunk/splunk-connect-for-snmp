b41e5949bde1beda0b508b8c502818bd273b49da
omrozowicz-splunk <omrozowicz@splunk.com>
2022-03-01 10:56:00 +0100

    test: add test of traps v3

--- integration_tests/test_trap_integration.py
+++ integration_tests/test_trap_integration.py
@@ -18,7 +18,7 @@ import time
 
 from pysnmp.hlapi import *
 
-from integration_tests.splunk_test_utils import splunk_single_search
+from integration_tests.splunk_test_utils import splunk_single_search, create_v3_secrets, update_traps
 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +27,24 @@ def send_trap(host, port, object_identity, mib_to_load, *var_binds):
     iterator = sendNotification(
         SnmpEngine(),
         CommunityData("public", mpModel=0),
+        UdpTransportTarget((host, port)),
+        ContextData(),
+        "trap",
+        NotificationType(ObjectIdentity(object_identity))
+        .addVarBinds(*var_binds)
+        .loadMibs(mib_to_load),
+    )
+
+    error_indication, error_status, error_index, var_binds = next(iterator)
+
+    if error_indication:
+        logger.error(f"{error_indication}")
+
+
+def send_v3_trap(host, port, object_identity, mib_to_load, *var_binds):
+    iterator = sendNotification(
+        SnmpEngine(OctetString(hexValue='80003a8c04')),
+        UsmUserData('snmp-poller', 'PASSWORD1', 'PASSWORD1', authProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 1, 3), privProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 2, 4)),
         UdpTransportTarget((host, port)),
         ContextData(),
         "trap",
@@ -135,6 +153,27 @@ def test_loading_mibs(request, setup_splunk):
     time.sleep(2)
 
     search_query = """search index=netops "SNMPv2-MIB.snmpTrapOID.value"="AVAMAR-MCS-MIB::eventTrap"  """
+
+    result_count, events_count = splunk_single_search(setup_splunk, search_query)
+
+    assert result_count == 1
+
+
+def test_trap_v3(request, setup_splunk):
+    trap_external_ip = request.config.getoption("trap_external_ip")
+    create_v3_secrets()
+    update_traps(["secretv4"])
+    logger.info(f"I have: {trap_external_ip}")
+
+    time.sleep(2)
+    # send trap
+    varbind1 = ('1.3.6.1.2.1.1.1.0', OctetString('test_trap_v3'))
+    send_v3_trap(trap_external_ip, 162, "1.3.6.1.2.1.2.1", "SNMPv2-MIB", varbind1)
+
+    # wait for the message to be processed
+    time.sleep(2)
+
+    search_query = """search index=netops "SNMPv2-MIB.sysDescr.value"="test_trap_v3"  """
 
     result_count, events_count = splunk_single_search(setup_splunk, search_query)
 
