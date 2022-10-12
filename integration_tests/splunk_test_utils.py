542e92b6ff8f21075577ff176a3fb10357ea3084
harshit-splunk <hvaghani@splunk.com>
2022-10-12 16:27:55 +0530

    add sleep [run-int-tests]

--- integration_tests/splunk_test_utils.py
+++ integration_tests/splunk_test_utils.py
@@ -148,6 +148,8 @@ def upgrade_helm(yaml_files):
     os.system(
         f"sudo microk8s helm3 upgrade --install snmp {files_string} ~/splunk-connect-for-snmp/charts/splunk-connect-for-snmp --namespace=sc4snmp --create-namespace"
     )
+    # temparorily added
+    time.sleep(30)
 
 
 def create_v3_secrets(
