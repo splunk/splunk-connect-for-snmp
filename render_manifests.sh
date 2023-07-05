76f6892411aaf3d680991784a3bd2195c0f86755
Przemysław Grenda <pgrenda@splunk.com>
2023-07-05 11:10:18 +0200

    fix: PR comments fixes, more generic approach for rendering test cases add rendered files to PR scope.(#732)

--- render_manifests.sh
+++ render_manifests.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-TEST_CASES=("only_polling" "only_traps" "autoscaling_enabled" "autoscaling_enabled_deprecated")
+DIR="rendered"
+prefix="$DIR/values_"
+suffix=".yaml"
+declare -a TEST_CASES=()
+
+for file in $prefix* ; do
+    if [ -f "$file" ] ; then
+        filename=${file#"$prefix"}    # Remove prefix.
+        filename=${filename%"$suffix"} # Remove suffix.
+        TEST_CASES+=("$filename")      # Append to array.
+    fi
+done
+
 for test_case in "${TEST_CASES[@]}"
-  do
-      VALUES_FILE=rendered/values_"${test_case}".yaml
-      MANIFEST_DIR=rendered/manifests/tests_"${test_case}"
-      helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
-      rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
-  done
+do
+    VALUES_FILE="$DIR/values_${test_case}.yaml"
+    MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
+    helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
+    rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
+done
