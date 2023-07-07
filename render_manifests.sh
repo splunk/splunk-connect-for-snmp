1d3c5d40eabb1019ff084d8b869a81466e5c5359
Przemysław Grenda <pgrenda@splunk.com>
2023-07-07 10:09:50 +0200

    fix: pr review fixed: different approuch for deployment temlating, render manifest and makefile changes (#732)

--- render_manifests.sh
+++ render_manifests.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+#!/bin/bash
 
 DIR="rendered"
 prefix="$DIR/values_"
 suffix=".yaml"
 declare -a TEST_CASES=()
 
-for file in $prefix* ; do
-    if [ -f "$file" ] ; then
-        filename=${file#"$prefix"}    # Remove prefix.
-        filename=${filename%"$suffix"} # Remove suffix.
-        TEST_CASES+=("$filename")      # Append to array.
-    fi
+for file in $prefix*; do
+  if [ -f "$file" ]; then
+    filename=${file#"$prefix"}     # Remove prefix.
+    filename=${filename%"$suffix"} # Remove suffix.
+    TEST_CASES+=("$filename")      # Append to array.
+  fi
 done
 
-for test_case in "${TEST_CASES[@]}"
-do
-    VALUES_FILE="$DIR/values_${test_case}.yaml"
-    MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
-    helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
-    rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
+for test_case in "${TEST_CASES[@]}"; do
+  VALUES_FILE="$DIR/values_${test_case}.yaml"
+  MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
+  helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
+  rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
 done
