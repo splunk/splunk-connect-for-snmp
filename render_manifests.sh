112f7e0c36bb03b6faab1f524a5e1a3e214d453d
ajasnosz <139114006+ajasnosz@users.noreply.github.com>
2023-07-26 14:44:41 +0200

    chore: set unchangeable tag in rendered (#768)
    * chore: update versioning
    
    * chore: update version rendering

--- render_manifests.sh
+++ render_manifests.sh
@@ -18,5 +18,7 @@ for test_case in "${TEST_CASES[@]}"; do
   VALUES_FILE="$DIR/values_${test_case}.yaml"
   MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
   helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
+  APPVERSION=`sed -nr 's/appVersion: "(.+)"/\1/p' charts/splunk-connect-for-snmp/Chart.yaml` #Find current app version
+  sed -i '' -e 's/'"$APPVERSION"'/CURRENT-VERSION/g' $(find rendered/ -type f) #Replace version with default var
   rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
 done
