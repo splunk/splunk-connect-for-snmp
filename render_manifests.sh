3cbe9e0a503b5af57e6717fd6fdb4792ed7998d9
ajasnosz <ajasnosz@splunk.com>
2024-04-25 12:35:02 +0200

    chore: resolve shell issues

--- render_manifests.sh
+++ render_manifests.sh
@@ -6,7 +6,7 @@ prefix="$DIR/values_"
 suffix=".yaml"
 declare -a TEST_CASES=()
 
-for file in $prefix*; do
+for file in "$prefix"*; do
   if [ -f "$file" ]; then
     filename=${file#"$prefix"}     # Remove prefix.
     filename=${filename%"$suffix"} # Remove suffix.
@@ -18,7 +18,10 @@ for test_case in "${TEST_CASES[@]}"; do
   VALUES_FILE="$DIR/values_${test_case}.yaml"
   MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
   helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
-  APPVERSION=`sed -nr 's/appVersion: "(.+)"/\1/p' charts/splunk-connect-for-snmp/Chart.yaml` #Find current app version
-  sed -i '' -e 's/'"$APPVERSION"'/CURRENT-VERSION/g' $(find rendered/ -type f) #Replace version with default var
+  APPVERSION="$(sed -nr 's/appVersion: "(.+)"/\1/p' charts/splunk-connect-for-snmp/Chart.yaml)" #Find current app version
+  FILES=$(find rendered/ -type f)
+  for f in $FILES; do
+    sed -i '' -e 's/'"$APPVERSION"'/CURRENT-VERSION/g' "$f" #Replace version with default var
+  done
   rm -rf "${MANIFEST_DIR}"/splunk-connect-for-snmp/charts
 done
