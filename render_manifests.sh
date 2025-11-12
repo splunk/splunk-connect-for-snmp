4edca48e79d129e851c232a4c03e46993bf609d9
omrozowicz-splunk <omrozowicz@splunk.com>
2025-11-12 12:44:09 +0100

    fix: add annotations with checksum to all components and run make render

--- render_manifests.sh
+++ render_manifests.sh
@@ -17,7 +17,7 @@ done
 for test_case in "${TEST_CASES[@]}"; do
   VALUES_FILE="$DIR/values_${test_case}.yaml"
   MANIFEST_DIR="$DIR/manifests/tests_${test_case}"
-  helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp --debug
+  helm template --values "${VALUES_FILE}" --output-dir "${MANIFEST_DIR}" -n default charts/splunk-connect-for-snmp
   APPVERSION="$(sed -nr 's/appVersion: "(.+)"/\1/p' charts/splunk-connect-for-snmp/Chart.yaml)" #Find current app version
   FILES=$(find rendered/ -type f)
   for f in $FILES; do
