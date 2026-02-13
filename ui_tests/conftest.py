e2a3dbf3c2ad2142eb80c8543adc4b43f641c7b6
pkalevar16 <pkalevar@splunk.com>
2026-02-13 10:31:34 +0000

    test: review comment fixed logs [run-int-tests][run-ui-tests]

--- ui_tests/conftest.py
+++ ui_tests/conftest.py
@@ -19,6 +19,17 @@ def pytest_addoption(parser):
     )
 
 
+@pytest.fixture(scope="function")
+def setup(request):
+    config = {}
+    host = request.config.getoption("--splunk-host")
+    config["splunkd_url"] = "https://" + host + ":8089"
+    config["splunk_user"] = request.config.getoption("--splunk-user")
+    config["splunk_password"] = request.config.getoption("--splunk-password")
+    config["device_simulator"] = request.config.getoption("device-simulator")
+    return config
+
+
 def pytest_unconfigure():
     logger.info("Closing Web Driver")
     WebDriverFactory.close_driver()
