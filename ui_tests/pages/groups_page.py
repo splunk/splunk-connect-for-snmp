b74096df84c99a605536fa9414d6dbb44b21b93d
pkalevar16 <pkalevar@splunk.com>
2026-02-18 10:22:39 +0000

    test: review comment fixed logs [run-int-tests][run-ui-tests]

--- ui_tests/pages/groups_page.py
+++ ui_tests/pages/groups_page.py
@@ -15,7 +15,7 @@ class GroupsPage:
         groups_table_xpath = "//div[@data-test='sc4snmp:group-table']"
         groups_container = driver.find_element(By.XPATH, groups_table_xpath)
         return groups_container.is_displayed()
-    
+
     def click_add_new_group_button(self):
         logger.info("Click add new group button")
         add_group_button_xpath = "//button[@data-test='sc4snmp:new-item-button']//span"
